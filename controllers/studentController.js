b628f2de6ca8b68cb940dc58e8aa632fa9ddb663
gangadharteli18 <gangadharteli27@gmail.com>
2026-03-28 10:59:14 +0530

    Collective commit

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,99 +1,79 @@
 const db=require('../utils/db-connection')
+const student=require('../models/student')
 
-const addstudent=(req,res)=>{
-    const {name,email,age}=req.body;
-    const addQuery=`insert into students(name,email,age) 
-    values(?,?,?)`
-
-    db.execute(addQuery,[ name,email,age],(err)=>{
-        if(err){
-            console.log(err)
-            res.status(500).send(err.message)
-            
-            return;
-        }
-        console.log("student added")
-        res.status(200).send(`student ${name} added`)
-    })
+const addstudent=async(req,res)=>{
+    try {
+        const {name,email}=req.body;
+        await student.create({
+            name:name,
+            email:email,
+          
+        })
+          res.status(200).send(`student ${name} added`)
+    } catch (error) {
+         res.status(500).send(error.message)
+         return;
+    }
+    
 
 }
 //update
-const updatestudent=(req,res)=>{
-    const id=req.params.id;
-    let fields=[];
-    let values=[];
-    const {name,email,age}=req.body;
-    if(name){
-        fields.push("name=?")
-        values.push(name);
-    }
-    if(email){
-        fields.push("email=?")
-         values.push(email);
-    }
-    if(age){
-        fields.push("age=?")
-         values.push(age);
-    }
-   
-    if (fields.length === 0) {
-        return res.status(400).send("No fields to update");
-    }
-    const updateQuery=`update  students set ${fields.join(", ")} where id=?`
-    values.push(id)
-
-    db.execute(updateQuery,values,(err,result)=>{
-        if(err){
-            console.log(err)
-            res.status(500).send(err.message)
-            return;
+const updatestudent=async(req,res)=>{
+    try {
+         const id=req.params.id;
+       const {name,email}=req.body;
+       const Student=await student.findByPk(id)
+        if(!Student){
+         res.status(404).send("student not found")
+         return;
         }
-        if(result.affectedRows===0){
-            res.status(404).send("student not found")
-            return;
-        }
-        console.log("student updated")
-        res.status(200).send(`student ${name} updated`)
-    })
+       if(name)Student.name=name;
+       if(email)Student.email=email;
+        await Student.save()
+ 
+       res.status(200).send(`student ${name} updated`)
+        
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
 
 }
 //delete
-const deleteStudent=(req,res)=>{
-    const id=req.params.id;
-    const deleteQuery=`DELETE FROM students where id=?`
-
-    db.execute(deleteQuery,[id],(err,result)=>{
-        if(err){
-            console.log(err)
-            res.status(500).send(err.message)
-           
-            return;
-        }
-        if(result.affectedRows===0){
+const deleteStudent=async(req,res)=>{
+    
+    try {
+        const id=req.params.id;
+        const Student=await student.destroy({
+           where :{
+            id:id
+           }
+        })
+        if(!Student){
             res.status(404).send("student not found")
-            return;
+         return;
         }
-        console.log("student deleted")
-        res.status(200).send(`student  deleted with id ${id}`)
-    })
+        res.status(200).send(`student deleted`)
+    } catch (error) {
+         res.status(500).send(error.message)
+    }
 
 }
 
 //to get all students
-const getStudent=(req,res)=>{
-    const getQuery=`select id,name from students`
-    db.execute(getQuery,(err,result)=>{
-         if(err){
-            console.log(err)
-            res.status(500).send(err.message)
-           
-            return;
+const getStudent=async(req,res)=>{
+    
+   try {
+        
+        const Student=await student.findAll()
+        if(Student.length===0){
+            res.status(404).send("student not found")
+         return;
         }
-        if(result.length===0){
-            res.status(200).send('No students yet')
-        }
-        res.status(200).send(result)
-    })
+          res.status(200).json(Student)
+    } catch (error) {
+         res.status(500).send(error.message)
+    }
+
 }
 
 module.exports={
